27702f415d921e8e66460f244fe0ecced606218c
xmousez <xmousez@163.com>
2017-05-26 12:00:24 +0800

    加了一点注释，并且增加了OnApplication...和OnMouse...的绑定

--- Assets/EZFramework/XLuaExtension/LuaBehaviour.cs
+++ Assets/EZFramework/XLuaExtension/LuaBehaviour.cs
@@ -11,13 +11,18 @@ using XLua;
 
 namespace EZFramework
 {
+    /* 
+     * 因为在Lua中可能出现多个GameObject绑定了同一个LuaTable，
+     * 也就是不同GameObject的Message可能调用了同一个LuaFunction，
+     * 所以在每个方法上我都将GameObject传了回去；
+     */
     public abstract class TLuaBehaviour<T> : MonoBehaviour
         where T : TLuaBehaviour<T>
     {
         public LuaTable self;
         public static void Bind(GameObject obj, LuaTable self)
         {
-            T behaviour = obj.AddComponent<T>();
+            T behaviour = obj.AddComponent<T>();    // 在这一句实际上Awake已经执行了，所以Awake方法无法被动态绑定；
             behaviour.self = self;
         }
         public static void Remove(GameObject obj)
@@ -133,6 +138,76 @@ namespace EZFramework
         }
     }
 
+    [LuaCallCSharp]
+    public class LuaApplicationBehaviour : TLuaBehaviour<LuaApplicationBehaviour>
+    {
+        private Action<LuaTable, GameObject, bool> luaOnApplicationFocus;
+        private Action<LuaTable, GameObject, bool> luaOnApplicationPause;
+        void Start()
+        {
+            self.Get("LuaOnApplicationFocus", out luaOnApplicationFocus);
+            self.Get("LuaOnApplicationPause", out luaOnApplicationPause);
+        }
+        void OnApplicationFocus(bool focusStatus)
+        {
+            if (luaOnApplicationFocus != null) luaOnApplicationFocus(self, gameObject, focusStatus);
+        }
+        void OnApplicationPause(bool pauseStatus)
+        {
+            if (luaOnApplicationPause != null) luaOnApplicationPause(self, gameObject, pauseStatus);
+        }
+    }
+
+    [LuaCallCSharp]
+    public class LuaMouseBehaviour : TLuaBehaviour<LuaMouseBehaviour>
+    {
+        private Action<LuaTable, GameObject> luaOnMouseEnter;
+        private Action<LuaTable, GameObject> luaOnMouseOver;
+        private Action<LuaTable, GameObject> luaOnMouseDown;
+        private Action<LuaTable, GameObject> luaOnMouseDrag;
+        private Action<LuaTable, GameObject> luaOnMouseUp;
+        private Action<LuaTable, GameObject> luaOnMouseExit;
+        private Action<LuaTable, GameObject> luaOnMouseUpAsButton;
+        void Start()
+        {
+            self.Get("LuaOnMouseEnter", out luaOnMouseEnter);
+            self.Get("LuaOnMouseOver", out luaOnMouseOver);
+            self.Get("LuaOnMouseDown", out luaOnMouseDown);
+            self.Get("LuaOnMouseDrag", out luaOnMouseDrag);
+            self.Get("LuaOnMouseUp", out luaOnMouseUp);
+            self.Get("LuaOnMouseExit", out luaOnMouseExit);
+            self.Get("LuaOnMouseUpAsButton", out luaOnMouseUpAsButton);
+        }
+        void OnMouseEnter()
+        {
+            if (luaOnMouseEnter != null) luaOnMouseEnter(self, gameObject);
+        }
+        void OnMouseOver()
+        {
+            if (luaOnMouseOver != null) luaOnMouseOver(self, gameObject);
+        }
+        void OnMouseDown()
+        {
+            if (luaOnMouseDown != null) luaOnMouseDown(self, gameObject);
+        }
+        void OnMouseDrag()
+        {
+            if (luaOnMouseDrag != null) luaOnMouseDrag(self, gameObject);
+        }
+        void OnMouseUp()
+        {
+            if (luaOnMouseUp != null) luaOnMouseUp(self, gameObject);
+        }
+        void OnMouseExit()
+        {
+            if (luaOnMouseExit != null) luaOnMouseExit(self, gameObject);
+        }
+        void OnMouseUpAsButton()
+        {
+            if (luaOnMouseUpAsButton != null) luaOnMouseUpAsButton(self, gameObject);
+        }
+    }
+
     public static class XLuaBehaviourGen
     {
         [CSharpCallLua]
@@ -141,6 +216,7 @@ namespace EZFramework
             typeof(System.Action<LuaTable, GameObject>),
             typeof(System.Action<LuaTable, GameObject, Collision>),
             typeof(System.Action<LuaTable, GameObject, Collider>),
+            typeof(System.Action<LuaTable, GameObject, bool>),
         };
     }
 }
