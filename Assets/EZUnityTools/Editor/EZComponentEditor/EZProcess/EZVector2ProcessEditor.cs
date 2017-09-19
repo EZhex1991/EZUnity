e2f82328158a30d1ff876fe66f5460125ec98d41
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2017-09-19 16:09:26 +0800

    流程动画的自定义编辑器

---
+++ Assets/EZUnityTools/Editor/EZComponentEditor/EZProcess/EZVector2ProcessEditor.cs
@@ -0,0 +1,17 @@
+/*
+ * Author:      熊哲
+ * CreateTime:  9/12/2017 3:35:11 PM
+ * Description:
+ * 
+*/
+using EZComponent.EZProcess;
+using UnityEditor;
+
+namespace EZComponentEditor.EZProcess
+{
+    [CustomEditor(typeof(EZVector2Process), true), CanEditMultipleObjects]
+    public class EZVector2ProcessEditor : _EZProcessEditor
+    {
+
+    }
+}
---
+++ Assets/EZUnityTools/Editor/EZComponentEditor/EZProcess/EZVector3ProcessEditor.cs
@@ -0,0 +1,17 @@
+/*
+ * Author:      熊哲
+ * CreateTime:  9/12/2017 11:45:43 AM
+ * Description:
+ * 
+*/
+using EZComponent.EZProcess;
+using UnityEditor;
+
+namespace EZComponentEditor.EZProcess
+{
+    [CustomEditor(typeof(EZVector3Process), true), CanEditMultipleObjects]
+    public class EZVector3ProcessEditor : _EZProcessEditor
+    {
+
+    }
+}
