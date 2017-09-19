c785ab25db3b91d07c1c37e4151a582f0901a1a4
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2017-09-19 16:30:05 +0800

    编辑器扩展的菜单

---
+++ Assets/EZUnityTools/Editor/EZUnityEditor/MenuItems.cs
@@ -0,0 +1,50 @@
+/*
+ * Author:      熊哲
+ * CreateTime:  2/14/2017 3:58:53 PM
+ * Description:
+ * 
+*/
+using UnityEditor;
+
+namespace EZUnityEditor
+{
+    public class MenuItems
+    {
+        private const string ROOT_NAME = "EZUnityEditor";
+
+        [MenuItem(ROOT_NAME + "/EZScriptTemplate", priority = 1)]
+        private static void EZScriptTemplate()
+        {
+            EditorWindow.GetWindow<EZScriptTemplateEditorWindow>("EZScriptTemplate").Show();
+        }
+
+        [MenuItem(ROOT_NAME + "/EZKeystore", priority = 2)]
+        private static void EZKeystore()
+        {
+            EditorWindow.GetWindow<EZKeystoreEditorWindow>("EZKeystore").Show();
+        }
+
+        [MenuItem(ROOT_NAME + "/EZBundle", priority = 3)]
+        private static void EZBundle()
+        {
+            EditorWindow.GetWindow<EZBundleEditorWindow>("EZBundle").Show();
+        }
+
+        [MenuItem(ROOT_NAME + "/EZRename", priority = 4)]
+        private static void EZRename()
+        {
+            EditorWindow.GetWindow<EZRenameEditorWindow>("EZRename").Show();
+        }
+
+        [MenuItem(ROOT_NAME + "/EZAsset Generator/Material", priority = 6)]
+        private static void GenerateMaterial()
+        {
+            Selection.activeObject = EZAssetGenerator.GenerateMaterial();
+        }
+        [MenuItem(ROOT_NAME + "/EZAsset Generator/TextAsset", priority = 7)]
+        private static void GenerateTextAsset()
+        {
+            Selection.activeObject = EZAssetGenerator.GenerateTextAsset();
+        }
+    }
+}
