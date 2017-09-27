61ce00e9683a5f34ffb4835a05f55e31d999bf37
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2017-09-27 11:27:56 +0800

    用来替代LuaBehaviour，把消息封装成了event，闭包换到了lua侧实现

---
+++ Assets/EZFramework/XLuaExtension/LuaMessage/ActivityMessage.cs
@@ -0,0 +1,43 @@
+/*
+ * Author:      熊哲
+ * CreateTime:  9/26/2017 6:07:04 PM
+ * Description:
+ * 
+*/
+using UnityEngine;
+
+namespace EZFramework.XLuaExtension
+{
+    [DisallowMultipleComponent]
+    public class ActivityMessage : _Message<ActivityMessage>
+    {
+        public class ActivityEvent : OnMessageEvent { }
+
+        public ActivityEvent awake = new ActivityEvent();
+        public ActivityEvent start = new ActivityEvent();
+        public ActivityEvent onEnable = new ActivityEvent();
+        public ActivityEvent onDisable = new ActivityEvent();
+        public ActivityEvent onDestroy = new ActivityEvent();
+
+        void Awake()
+        {
+            awake.Invoke();
+        }
+        void Start()
+        {
+            start.Invoke();
+        }
+        void OnEnable()
+        {
+            onEnable.Invoke();
+        }
+        void OnDisable()
+        {
+            onDisable.Invoke();
+        }
+        void OnDestroy()
+        {
+            onDestroy.Invoke();
+        }
+    }
+}
