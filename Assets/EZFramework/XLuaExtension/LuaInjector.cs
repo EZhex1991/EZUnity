9bdf91fe90a76b162e868016bb9313591482933a
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2017-11-23 17:01:19 +0800

    添加注释，个人不建议在C#上构造空表

--- Assets/EZFramework/XLuaExtension/LuaInjector.cs
+++ Assets/EZFramework/XLuaExtension/LuaInjector.cs
@@ -29,14 +29,14 @@ namespace EZFramework.XLuaExtension
                 Injection pair = injections[i];
                 if (pair.key.Contains("."))
                 {
-                    self.SetInPath(pair.key, pair.value);
+                    self.SetInPath(pair.key, pair.value);   // SetInPath必须保证Path不为nil
                 }
                 else if (pair.key.Contains("#"))
                 {
                     string[] info = pair.key.Split('#');
                     string tableName = info[0];
                     int index = System.Convert.ToInt32(info[1]);
-                    LuaTable table = self.Get<LuaTable>(tableName);
+                    LuaTable table = self.Get<LuaTable>(tableName); // 与SetInPath规则保持一致，此处table不能为nil
                     table.Set(index, pair.value);
                 }
                 else
