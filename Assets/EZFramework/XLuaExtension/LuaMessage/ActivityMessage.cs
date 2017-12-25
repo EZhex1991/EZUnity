222ad27d75afe3d097d15433d4da8396d8fa4747
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2017-12-25 18:27:43 +0800

    Awake移除

--- Assets/EZFramework/XLuaExtension/LuaMessage/ActivityMessage.cs
+++ Assets/EZFramework/XLuaExtension/LuaMessage/ActivityMessage.cs
@@ -13,16 +13,11 @@ namespace EZFramework.XLuaExtension
     {
         public class ActivityEvent : OnMessageEvent { }
 
-        public ActivityEvent awake = new ActivityEvent();
         public ActivityEvent start = new ActivityEvent();
         public ActivityEvent onEnable = new ActivityEvent();
         public ActivityEvent onDisable = new ActivityEvent();
         public ActivityEvent onDestroy = new ActivityEvent();
 
-        void Awake()
-        {
-            awake.Invoke();
-        }
         void Start()
         {
             start.Invoke();
