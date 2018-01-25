101d42ce5a2bcab353ebca493455a09c09a90e17
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2018-01-25 17:16:55 +0800

    菜单项修改

--- Assets/EZFramework/Editor/MenuItems.cs
+++ Assets/EZFramework/Editor/MenuItems.cs
@@ -31,28 +31,28 @@ namespace EZFrameworkEditor
         [MenuItem(ROOT_NAME + "/Copy Lua To Text", priority = 20)]
         public static void LuaToTxt()
         {
-            EZFrameworkUtility.LuaToTxt();
+            EZFrameworkEditorUtility.LuaToTxt();
         }
         [MenuItem(ROOT_NAME + "/Clear Lua Text", priority = 21)]
         public static void ClearLuaTxt()
         {
-            EZFrameworkUtility.ClearLuaTxt();
+            EZFrameworkEditorUtility.ClearLuaTxt();
         }
 
         [MenuItem(ROOT_NAME + "/Clear Persistent Path", priority = 50)]
         public static void ClearPersistent()
         {
-            EZFrameworkUtility.ClearPersistent();
+            EZFrameworkEditorUtility.ClearPersistent();
         }
         [MenuItem(ROOT_NAME + "/Clear StreamingAssets Path", priority = 51)]
         public static void ClearStreamingAssets()
         {
-            EZFrameworkUtility.ClearStreamingAssets();
+            EZFrameworkEditorUtility.ClearStreamingAssets();
         }
         [MenuItem(ROOT_NAME + "/Clear PlayerPrefs", priority = 52)]
         public static void ClearPlayerPrefs()
         {
-            EZFrameworkUtility.ClearPlayerPrefs();
+            EZFrameworkEditorUtility.ClearPlayerPrefs();
         }
     }
 }
