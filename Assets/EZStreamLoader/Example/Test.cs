7d19fbce1aee60c65e2d231b5992e102028b987c
zackejohn@punkmelon_pc <ezhex1991@outlook.com>
2018-01-26 12:22:18 +0800

    用来同步加载StreamingAssets的小例子

---
+++ Assets/EZStreamLoader/Example/Test.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author:      #AUTHORNAME#
+ * CreateTime:  #CREATETIME#
+ * Description:
+ * 
+*/
+using UnityEngine;
+using System.IO;
+
+public class Test : MonoBehaviour
+{
+    string path = "files.txt";
+    string text = "";
+
+    void StreamLoad(string path)
+    {
+        text = EZStreamLoader.LoadText(path);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 300, 80), "Load " + path))
+        {
+            StreamLoad(path);
+        }
+        GUI.TextField(new Rect(10, 200, 500, 290), text);
+    }
+}
