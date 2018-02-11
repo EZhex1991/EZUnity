c13e367729dcec72b74a64fb7d4077391ffeda16
EZhex1991 <ezhex1991@outlook.com>
2018-02-11 02:59:36 +0000

    assign priority to menu items

--- Assets/EZFramework/Editor/MenuItems.cs
+++ Assets/EZFramework/Editor/MenuItems.cs
@@ -13,8 +13,9 @@ namespace EZFrameworkEditor
     public class MenuItems
     {
         private const string ROOT_NAME = "EZFramework";
+        private const int PRIORITY = 11000;
 
-        [MenuItem(ROOT_NAME + "/Settings", priority = 0)]
+        [MenuItem(ROOT_NAME + "/Settings", false, PRIORITY + 1)]
         public static void EZSettings()
         {
             string assetPath = EZFrameworkSettings.AssetDirPath + EZFrameworkSettings.AssetName + ".asset";
@@ -28,28 +29,28 @@ namespace EZFrameworkEditor
             Selection.activeObject = target;
         }
 
-        [MenuItem(ROOT_NAME + "/Copy Lua To Text", priority = 20)]
+        [MenuItem(ROOT_NAME + "/Copy Lua To Text", false, PRIORITY + 101)]
         public static void LuaToTxt()
         {
             EZFrameworkEditorUtility.LuaToTxt();
         }
-        [MenuItem(ROOT_NAME + "/Clear Lua Text", priority = 21)]
+        [MenuItem(ROOT_NAME + "/Clear Lua Text", false, PRIORITY + 102)]
         public static void ClearLuaTxt()
         {
             EZFrameworkEditorUtility.ClearLuaTxt();
         }
 
-        [MenuItem(ROOT_NAME + "/Clear Persistent Path", priority = 50)]
+        [MenuItem(ROOT_NAME + "/Clear Persistent Path", false, PRIORITY + 201)]
         public static void ClearPersistent()
         {
             EZFrameworkEditorUtility.ClearPersistent();
         }
-        [MenuItem(ROOT_NAME + "/Clear StreamingAssets Path", priority = 51)]
+        [MenuItem(ROOT_NAME + "/Clear StreamingAssets Path", false, PRIORITY + 202)]
         public static void ClearStreamingAssets()
         {
             EZFrameworkEditorUtility.ClearStreamingAssets();
         }
-        [MenuItem(ROOT_NAME + "/Clear PlayerPrefs", priority = 52)]
+        [MenuItem(ROOT_NAME + "/Clear PlayerPrefs", false, PRIORITY + 203)]
         public static void ClearPlayerPrefs()
         {
             EZFrameworkEditorUtility.ClearPlayerPrefs();
--- Assets/EZUnityTools/Editor/EZUnityEditor/MenuItems.cs
+++ Assets/EZUnityTools/Editor/EZUnityEditor/MenuItems.cs
@@ -11,58 +11,57 @@ namespace EZUnityEditor
     public class MenuItems
     {
         private const string ROOT_NAME = "EZUnityEditor";
+        private const int PRIORITY = 12000;
 
-        [MenuItem(ROOT_NAME + "/EZRename", false, 1)]
+        [MenuItem(ROOT_NAME + "/EZRename", false, PRIORITY + 1)]
         private static void EZRename()
         {
             EditorWindow.GetWindow<EZRenameEditorWindow>("EZRename").Show();
         }
-
-        [MenuItem(ROOT_NAME + "/EZPlayerPrefsEditor", false, 2)]
+        [MenuItem(ROOT_NAME + "/EZPlayerPrefsEditor", false, PRIORITY + 2)]
         private static void PlayerPrefsEditor()
         {
             EditorWindow.GetWindow<EZPlayerPrefsEditor>("EZPlayerPrefsEditor").Show();
         }
-
-        [MenuItem(ROOT_NAME + "/EZScriptTemplate", false, 3)]
+        [MenuItem(ROOT_NAME + "/EZScriptTemplate", false, PRIORITY + 3)]
         private static void EZScriptTemplate()
         {
             EditorWindow.GetWindow<EZScriptTemplateEditorWindow>("EZScriptTemplate").Show();
         }
 
-        [MenuItem(ROOT_NAME + "/EZProjectSettings/EZKeystore", false, 100)]
+        [MenuItem(ROOT_NAME + "/EZProjectSettings/EZKeystore", false, PRIORITY + 101)]
         private static void EZKeystore()
         {
             Selection.activeObject = EZScriptableObject.Load<EZKeystoreObject>(EZKeystoreObject.AssetName);
         }
-        [MenuItem(ROOT_NAME + "/EZProjectSettings/Include Built-in Shaders", false, 101)]
+        [MenuItem(ROOT_NAME + "/EZProjectSettings/Include Built-in Shaders", false, PRIORITY + 102)]
         private static void IncludeBuiltinShaders()
         {
             EZGraphicsSettings.IncludeBuiltinShaders();
         }
 
-        [MenuItem(ROOT_NAME + "/EZAssetGenerator/Material", false, 110)]
+        [MenuItem(ROOT_NAME + "/EZAssetGenerator/Material", false, PRIORITY + 111)]
         private static void GenerateMaterial()
         {
             Selection.activeObject = EZAssetGenerator.GenerateMaterial();
         }
-        [MenuItem(ROOT_NAME + "/EZAssetGenerator/TextAsset", false, 111)]
+        [MenuItem(ROOT_NAME + "/EZAssetGenerator/TextAsset", false, PRIORITY + 112)]
         private static void GenerateTextAsset()
         {
             Selection.activeObject = EZAssetGenerator.GenerateTextAsset();
         }
 
-        [MenuItem(ROOT_NAME + "/EZBundle/EZBundle", false, 120)]
+        [MenuItem(ROOT_NAME + "/EZBundle/EZBundle", false, PRIORITY + 121)]
         private static void EZBundle()
         {
             Selection.activeObject = EZScriptableObject.Load<EZBundleObject>(EZBundleObject.AssetName);
         }
-        [MenuItem(ROOT_NAME + "/EZBundle/EZBundleManager", false, 121)]
+        [MenuItem(ROOT_NAME + "/EZBundle/EZBundleManager", false, PRIORITY + 122)]
         private static void EZBundleManager()
         {
             EditorWindow.GetWindow<EZBundleManager>("EZBundleManager").Show();
         }
-        [MenuItem(ROOT_NAME + "/EZBundle/EZBundleViewer", false, 122)]
+        [MenuItem(ROOT_NAME + "/EZBundle/EZBundleViewer", false, PRIORITY + 123)]
         private static void EZBundleViewer()
         {
             EditorWindow.GetWindow<EZBundleViewer>("EZBundleViewer").Show();
