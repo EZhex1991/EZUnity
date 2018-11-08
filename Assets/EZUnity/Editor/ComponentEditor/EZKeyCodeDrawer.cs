afabf33e1f276d28bc5ac375f79c2a1a066d7c3d
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2018-11-08 16:17:41 +0800

    UnityEngine.KeyCode property drawer override

---
+++ Assets/EZUnity/Editor/ComponentEditor/EZKeyCodeDrawer.cs
@@ -0,0 +1,57 @@
+/* Author:          熊哲
+ * CreateTime:      2018-11-07 17:22:30
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace EZUnity
+{
+    [CustomPropertyDrawer(typeof(KeyCode))]
+    public class EZKeyCodeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            position.width -= 70;
+            EditorGUI.PropertyField(position, property);
+            position.x += position.width;
+            position.width = 65;
+            if (GUI.Button(position, "KeyCode", EditorStyles.miniButton))
+            {
+                DrawKeyCodeMenu((code) =>
+                {
+                    property.intValue = (int)code;
+                    property.serializedObject.ApplyModifiedProperties();
+                });
+            }
+            EditorGUI.EndProperty();
+        }
+        private void DrawKeyCodeMenu(GenericMenu.MenuFunction2 callback)
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                string menuName;
+                if (code.ToString().StartsWith("Joystick"))
+                {
+                    menuName = code.ToString();
+                    menuName = menuName.Substring(0, menuName.LastIndexOf("Button")) + "/" + menuName;
+                }
+                else if (Enum.IsDefined(typeof(EZKeyCode), (int)code))
+                {
+                    EZKeyCode ezKeyCode = (EZKeyCode)code;
+                    menuName = ezKeyCode.ToString().Replace("_", "/");
+                }
+                else
+                {
+                    menuName = "Other/" + code.ToString();
+                }
+                menu.AddItem(new GUIContent(menuName), false, callback, code);
+            }
+            menu.ShowAsContext();
+        }
+    }
+}
