ba38cb9b4308d9c1943e4a51edbca107473569e2
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2018-11-28 19:08:55 +0800

    DOTween examples

---
+++ Assets/XLuaExamples/09_DOTween/DOTweenTest.lua
@@ -0,0 +1,25 @@
+--[==[
+- Author:       熊哲
+- CreateTime:   2018-11-28 18:53:31
+- Organization: #ORGANIZATION#
+- Description:  
+--]==]
+local GameObject = CS.UnityEngine.GameObject
+local PrimitiveType = CS.UnityEngine.PrimitiveType
+
+local M = {}
+----- CODE -----
+local cube = GameObject.CreatePrimitive(PrimitiveType.Cube)
+cube.transform:DOScale(0.1, 1):OnComplete(function()
+    print(cube.name, "DOScale Complete")
+end)
+
+local sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere)
+local sequence = CS.DG.Tweening.DOTween.Sequence()
+sequence:Append(sphere.transform:DOMoveX(1, 1))
+sequence:Append(sphere.transform:DOScale(1.5, 1))
+sequence:OnComplete(function()
+    print(sphere.name, "Sequence Complete")
+end)
+----- CODE -----
+return M
