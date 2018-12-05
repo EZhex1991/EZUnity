d4e3e7c0d33a3dca0c9aa7b289082ca0d18feacc
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2018-12-05 19:19:02 +0800

    Implementation of serializable dictionary

---
+++ Assets/EZUnity/Script/Core/EZSerializableDictionary.cs
@@ -0,0 +1,48 @@
+/* Author:          熊哲
+ * CreateTime:      2018-11-29 17:17:10
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EZUnity
+{
+    [Serializable]
+    public class EZSerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        private List<TKey> m_Keys = new List<TKey>();
+        [SerializeField]
+        private List<TValue> m_Values = new List<TValue>();
+
+        private Dictionary<TKey, TValue> m_Dictionary = new Dictionary<TKey, TValue>();
+        public void OnBeforeSerialize()
+        {
+            m_Keys.Clear();
+            m_Values.Clear();
+            foreach (var pair in m_Dictionary)
+            {
+                m_Keys.Add(pair.Key);
+                m_Values.Add(pair.Value);
+            }
+        }
+        public void OnAfterDeserialize()
+        {
+            m_Dictionary.Clear();
+            for (int i = 0; i < m_Keys.Count; i++)
+            {
+                m_Dictionary.Add(m_Keys[i], m_Values[i]);
+            }
+        }
+
+        public Dictionary<TKey, TValue> dictionary { get { return m_Dictionary; } }
+
+        public TValue this[TKey key]
+        {
+            get { return m_Dictionary[key]; }
+            set { m_Dictionary[key] = value; }
+        }
+    }
+}
