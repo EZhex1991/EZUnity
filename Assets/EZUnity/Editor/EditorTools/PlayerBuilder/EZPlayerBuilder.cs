537e5dcb1f47ae000d3e006219763c2277561ddf
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2019-04-08 14:55:48 +0800

    builder player pipeline

---
+++ Assets/EZUnity/Editor/EditorTools/PlayerBuilder/EZPlayerBuilder.cs
@@ -0,0 +1,101 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2019-04-02 20:41:29
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace EZUnity
+{
+    [CreateAssetMenu(fileName = "EZPlayerBuilder", menuName = "EZUnity/EZPlayerBuilder", order = EZUtility.AssetOrder)]
+    public class EZPlayerBuilder : ScriptableObject
+    {
+        public bool configButDontBuild;
+
+        public string locationPathName;
+        public SceneAsset[] scenes;
+
+        public string companyName;
+        public string productName;
+        public string bundleIdentifier;
+        public string bundleVersion;
+        public int buildNumber;
+        public Texture2D icon;
+
+        public void Config(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget)
+        {
+            if (!string.IsNullOrEmpty(companyName)) PlayerSettings.companyName = companyName;
+            if (!string.IsNullOrEmpty(productName)) PlayerSettings.productName = productName;
+            if (!string.IsNullOrEmpty(bundleVersion)) PlayerSettings.bundleVersion = bundleVersion;
+            if (icon != null)
+            {
+                Texture2D[] icons = PlayerSettings.GetIconsForTargetGroup(buildTargetGroup, IconKind.Any);
+                for (int i = 0; i < icons.Length; i++)
+                {
+                    icons[i] = icon;
+                }
+                PlayerSettings.SetIconsForTargetGroup(buildTargetGroup, icons, IconKind.Any);
+            }
+            if (!string.IsNullOrEmpty(bundleIdentifier)) PlayerSettings.SetApplicationIdentifier(buildTargetGroup, bundleIdentifier);
+            switch (buildTargetGroup)
+            {
+                case BuildTargetGroup.Standalone:
+                    PlayerSettings.macOS.buildNumber = buildNumber.ToString();
+                    break;
+                case BuildTargetGroup.iOS:
+                    PlayerSettings.iOS.buildNumber = buildNumber.ToString();
+                    break;
+                case BuildTargetGroup.Android:
+                    PlayerSettings.Android.bundleVersionCode = buildNumber;
+                    break;
+            }
+
+        }
+        public void Execute(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget)
+        {
+            Config(buildTargetGroup, buildTarget);
+            if (configButDontBuild) return;
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            string[] scenePaths = new string[scenes.Length];
+            for (int i = 0; i < scenePaths.Length; i++)
+            {
+                scenePaths[i] = AssetDatabase.GetAssetPath(scenes[i]);
+            }
+            options.scenes = scenePaths;
+            if (string.IsNullOrEmpty(locationPathName))
+            {
+                locationPathName = EditorUtility.SaveFolderPanel("Choose Output Folder", "", "");
+                if (string.IsNullOrEmpty(locationPathName)) return;
+            }
+            switch (buildTarget)
+            {
+                case BuildTarget.StandaloneWindows:
+                    options.locationPathName = string.Format("{0}/{1}.exe", locationPathName, PlayerSettings.productName);
+                    break;
+                case BuildTarget.StandaloneWindows64:
+                    options.locationPathName = string.Format("{0}/{1}.exe", locationPathName, PlayerSettings.productName);
+                    break;
+                default:
+                    options.locationPathName = locationPathName;
+                    break;
+            }
+            options.target = buildTarget;
+            options.options = BuildOptions.None;
+            BuildReport report = BuildPipeline.BuildPlayer(options);
+            var summary = report.summary;
+            switch (summary.result)
+            {
+                case BuildResult.Failed:
+                    Debug.LogError("Build Failed");
+                    break;
+                case BuildResult.Succeeded:
+                    Debug.Log("Build Succeeded");
+                    Application.OpenURL(Directory.GetParent(summary.outputPath).FullName);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/EZUnity/Editor/EditorTools/PlayerBuilder/EZPlayerBuilderEditor.cs
@@ -0,0 +1,116 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2019-04-04 16:14:44
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using UnityEditor;
+using UnityEngine;
+
+namespace EZUnity
+{
+    [CustomEditor(typeof(EZPlayerBuilder)), CanEditMultipleObjects]
+    public class EZPlayerBuilderEditor : Editor
+    {
+        protected EZPlayerBuilder builder;
+
+        protected SerializedProperty m_ConfigButDontBuild;
+
+        protected SerializedProperty m_LocationPathName;
+        protected SerializedProperty m_Scenes;
+
+        protected SerializedProperty m_CompanyName;
+        protected SerializedProperty m_ProductName;
+        protected SerializedProperty m_BundleIdentifier;
+        protected SerializedProperty m_BundleVersion;
+        protected SerializedProperty m_BuildNumber;
+        protected SerializedProperty m_Icon;
+
+        protected void OnEnable()
+        {
+            builder = target as EZPlayerBuilder;
+            m_ConfigButDontBuild = serializedObject.FindProperty("configButDontBuild");
+            m_LocationPathName = serializedObject.FindProperty("locationPathName");
+            m_Scenes = serializedObject.FindProperty("scenes");
+            m_CompanyName = serializedObject.FindProperty("companyName");
+            m_ProductName = serializedObject.FindProperty("productName");
+            m_BundleIdentifier = serializedObject.FindProperty("bundleIdentifier");
+            m_BundleVersion = serializedObject.FindProperty("bundleVersion");
+            m_BuildNumber = serializedObject.FindProperty("buildNumber");
+            m_Icon = serializedObject.FindProperty("icon");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EZEditorGUIUtility.ScriptableObjectTitle(target as ScriptableObject, !serializedObject.isEditingMultipleObjects);
+            if (!serializedObject.isEditingMultipleObjects)
+            {
+                EditorGUILayout.LabelField("Build", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(m_ConfigButDontBuild);
+                if (GUILayout.Button("Android"))
+                {
+                    builder.Execute(BuildTargetGroup.Android, BuildTarget.Android);
+                    GUIUtility.ExitGUI();
+                }
+                if (GUILayout.Button("iOS"))
+                {
+                    builder.Execute(BuildTargetGroup.iOS, BuildTarget.iOS);
+                    GUIUtility.ExitGUI();
+                }
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    if (GUILayout.Button("Windows"))
+                    {
+                        builder.Execute(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+                        GUIUtility.ExitGUI();
+                    }
+                    if (GUILayout.Button("Windows64"))
+                    {
+                        builder.Execute(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+                        GUIUtility.ExitGUI();
+                    }
+                    if (GUILayout.Button("OSX"))
+                    {
+                        builder.Execute(BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX);
+                        GUIUtility.ExitGUI();
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                EditorGUILayout.Space();
+            }
+
+            EditorGUILayout.LabelField("Build Options", EditorStyles.boldLabel);
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_LocationPathName);
+                if (GUILayout.Button("...", EditorStyles.miniButton, new GUILayoutOption[] { GUILayout.Width(30) }))
+                {
+                    string path = EditorUtility.SaveFolderPanel("Choose Output Folder", "", "");
+                    if (!string.IsNullOrEmpty(path)) m_LocationPathName.stringValue = path;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.PropertyField(m_Scenes, true);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Player Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(m_CompanyName);
+            EditorGUILayout.PropertyField(m_ProductName);
+            EditorGUILayout.PropertyField(m_BundleIdentifier);
+            EditorGUILayout.PropertyField(m_BundleVersion);
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_BuildNumber);
+                if (GUILayout.Button("+", EditorStyles.miniButton, new GUILayoutOption[] { GUILayout.Width(30) }))
+                {
+                    m_BuildNumber.intValue++;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.PropertyField(m_Icon);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
