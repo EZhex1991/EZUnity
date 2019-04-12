b4f8630af8a714d6fb66899a9d6c39c91805a222
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2019-04-12 20:13:21 +0800

    EZTextureChannelModifier, use for remap Texture RGBA channels

---
+++ Assets/EZUnity/Editor/EditorTools/AssetGenerator/EZTextureChannelModifier.cs
@@ -0,0 +1,74 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2019-04-12 15:22:32
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using UnityEngine;
+
+namespace EZUnity
+{
+    [CreateAssetMenu(fileName = "EZTextureChannelModifier", menuName = "EZUnity/EZTextureChannelModifier", order = EZUtility.AssetOrder)]
+    public class EZTextureChannelModifier : EZTextureGenerator
+    {
+        public enum Channel { R, G, B, A }
+
+        public Texture2D referenceTexture;
+
+        public Channel channelR = Channel.R;
+        [EZCurveRange(0, 0, 1, 1)]
+        public AnimationCurve curveR = AnimationCurve.Linear(0, 0, 1, 1);
+        public Channel channelG = Channel.G;
+        [EZCurveRange(0, 0, 1, 1)]
+        public AnimationCurve curveG = AnimationCurve.Linear(0, 0, 1, 1);
+        public Channel channelB = Channel.B;
+        [EZCurveRange(0, 0, 1, 1)]
+        public AnimationCurve curveB = AnimationCurve.Linear(0, 0, 1, 1);
+        public Channel channelA = Channel.A;
+        [EZCurveRange(0, 0, 1, 1)]
+        public AnimationCurve curveA = AnimationCurve.Linear(0, 0, 1, 1);
+
+        protected override void SetPixels(Texture2D texture)
+        {
+            if (referenceTexture != null)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+                    for (int y = 0; y < texture.height; y++)
+                    {
+                        int coordX = Mathf.RoundToInt((float)x / (texture.width - 1) * (referenceTexture.width - 1));
+                        int coordY = Mathf.RoundToInt((float)y / (texture.height - 1) * (referenceTexture.height - 1));
+                        Color color = referenceTexture.GetPixel(coordX, coordY);
+                        Color newColor = new Color(
+                            FloatFromChannel(color, channelR, curveR),
+                            FloatFromChannel(color, channelG, curveG),
+                            FloatFromChannel(color, channelB, curveB),
+                            FloatFromChannel(color, channelA, curveA)
+                        );
+                        texture.SetPixel(x, y, newColor);
+                    }
+                }
+            }
+        }
+
+        public float FloatFromChannel(Color color, Channel channel, AnimationCurve curve)
+        {
+            float value = 1;
+            switch (channel)
+            {
+                case Channel.R:
+                    value = color.r;
+                    break;
+                case Channel.G:
+                    value = color.g;
+                    break;
+                case Channel.B:
+                    value = color.b;
+                    break;
+                case Channel.A:
+                    value = color.a;
+                    break;
+            }
+            return curve.Evaluate(value);
+        }
+    }
+}
---
+++ Assets/EZUnity/Editor/EditorTools/AssetGenerator/EZTextureChannelModifierEditor.cs
@@ -0,0 +1,67 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2019-04-12 16:13:31
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using UnityEditor;
+using UnityEngine;
+
+namespace EZUnity
+{
+    [CustomEditor(typeof(EZTextureChannelModifier))]
+    public class EZTextureChannelModifierEditor : EZTextureGeneratorEditor
+    {
+        private SerializedProperty m_ReferenceTexture;
+
+        private SerializedProperty m_ChannelR;
+        private SerializedProperty m_CurveR;
+        private SerializedProperty m_ChannelG;
+        private SerializedProperty m_CurveG;
+        private SerializedProperty m_ChannelB;
+        private SerializedProperty m_CurveB;
+        private SerializedProperty m_ChannelA;
+        private SerializedProperty m_CurveA;
+
+        protected override void GetProperties()
+        {
+            m_ReferenceTexture = serializedObject.FindProperty("referenceTexture");
+            m_ChannelR = serializedObject.FindProperty("channelR");
+            m_CurveR = serializedObject.FindProperty("curveR");
+            m_ChannelG = serializedObject.FindProperty("channelG");
+            m_CurveG = serializedObject.FindProperty("curveG");
+            m_ChannelB = serializedObject.FindProperty("channelB");
+            m_CurveB = serializedObject.FindProperty("curveB");
+            m_ChannelA = serializedObject.FindProperty("channelA");
+            m_CurveA = serializedObject.FindProperty("curveA");
+        }
+
+        protected override void DrawTextureSettings()
+        {
+            EditorGUILayout.PropertyField(m_ReferenceTexture);
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_ChannelR);
+                EditorGUILayout.PropertyField(m_CurveR, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_ChannelG);
+                EditorGUILayout.PropertyField(m_CurveG, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_ChannelB);
+                EditorGUILayout.PropertyField(m_CurveB, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(m_ChannelA);
+                EditorGUILayout.PropertyField(m_CurveA, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
