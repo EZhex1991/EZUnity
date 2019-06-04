02953f1e346b15118aea101e00b2df286b802e15
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2019-06-04 17:19:11 +0800

    bug fix on 2017.4

--- Assets/EZUnity/Editor/EditorTools/EZRegexTester.cs
+++ Assets/EZUnity/Editor/EditorTools/EZRegexTester.cs
@@ -32,11 +32,19 @@ namespace EZUnity
                 if (string.IsNullOrEmpty(regexString)) return;
                 foreach (Match match in Regex.Matches(inputString, regexString))
                 {
+#if UNITY_2018_1_OR_NEWER
                     EditorGUILayout.LabelField(string.Format("Match:\tIndex: {0}\tName: {1}\tValue: {2}", match.Index, match.Name, match.Value));
+#else
+                    EditorGUILayout.LabelField(string.Format("Match:\tIndex: {0}\tValue: {1}", match.Index, match.Value));
+#endif
                     EditorGUI.indentLevel++;
                     foreach (Group group in match.Groups)
                     {
+#if UNITY_2018_1_OR_NEWER
                         EditorGUILayout.LabelField(string.Format("Group:\tIndex: {0}\tName: {1}\tValue: {2}", group.Index, group.Name, group.Value));
+#else
+                        EditorGUILayout.LabelField(string.Format("Group:\tIndex: {0}\tValue: {1}", group.Index, group.Value));
+#endif
                         EditorGUI.indentLevel++;
                         foreach (Capture capture in group.Captures)
                         {
