866a3bf7637684fc96d28eba6ceb06b89458ee12
DESKTOP-88SBD3T\Zack John <ezhex1991@outlook.com>
2019-07-04 13:53:55 +0800

    reimport assets

---
+++ Assets/EZhex1991/EZUnity/Demo/Presets/_MaterialPreview.cs
@@ -0,0 +1,27 @@
+/* Author:          ezhex1991@outlook.com
+ * CreateTime:      2019-07-04 11:16:05
+ * Organization:    #ORGANIZATION#
+ * Description:     
+ */
+using UnityEngine;
+
+namespace EZhex1991.EZUnity
+{
+    public class _MaterialPreview : MonoBehaviour
+    {
+        public Renderer[] renderers;
+        public Material material;
+
+        private void OnValidate()
+        {
+            if (material != null)
+            {
+                gameObject.name = material.name;
+                foreach (Renderer renderer in renderers)
+                {
+                    if (renderer != null) renderer.sharedMaterial = material;
+                }
+            }
+        }
+    }
+}
