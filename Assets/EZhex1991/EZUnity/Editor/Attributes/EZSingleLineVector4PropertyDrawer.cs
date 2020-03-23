08894773faf32f38406928a3de1fd771f06cc1cf
Zack John <ezhex1991@outlook.com>
2020-03-23 15:58:32 +0800

    EZSingleLineVector4Attribute is suitable for Quaternion

--- Assets/EZhex1991/EZUnity/Editor/Attributes/EZSingleLineVector4PropertyDrawer.cs
+++ Assets/EZhex1991/EZUnity/Editor/Attributes/EZSingleLineVector4PropertyDrawer.cs
@@ -11,12 +11,25 @@ namespace EZhex1991.EZUnity
     [CustomPropertyDrawer(typeof(EZSingleLineVector4Attribute))]
     public class EZSingleLineVector4PropertyDrawer : PropertyDrawer
     {
+        private static GUIContent[] labels = new GUIContent[]
+        {
+            new GUIContent("X"),
+            new GUIContent("Y"),
+            new GUIContent("Z"),
+            new GUIContent("W"),
+        };
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
             if (property.propertyType == SerializedPropertyType.Vector4)
             {
                 property.vector4Value = EditorGUI.Vector4Field(position, label, property.vector4Value);
+            }
+            else if (property.propertyType == SerializedPropertyType.Quaternion)
+            {
+                property.Next(true);
+                EditorGUI.MultiPropertyField(position, labels, property, label);
             }
             else
             {
