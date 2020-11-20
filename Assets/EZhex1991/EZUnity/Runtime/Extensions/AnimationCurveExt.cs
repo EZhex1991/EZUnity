35ec09722f42998129d5a4a5ebf6999f5a420e66
Ethan Zack <ezhex1991@outlook.com>
2020-11-20 17:05:08 +0800

    replace keyframes by time

--- Assets/EZhex1991/EZUnity/Runtime/Extensions/AnimationCurveExt.cs
+++ Assets/EZhex1991/EZUnity/Runtime/Extensions/AnimationCurveExt.cs
@@ -20,14 +20,87 @@ namespace EZhex1991.EZUnity
                 curve.AddKey(other[i]);
             }
         }
-        public static float GetLastTime(this AnimationCurve curve)
+        public static void Replace(this AnimationCurve curve, AnimationCurve other)
         {
-            return curve.length > 0 ? curve.keys[curve.length - 1].time : 0;
+            if (other.length == 0) return;
+            if (other[0].time > other[other.length - 1].time)
+            {
+                Debug.LogErrorFormat("Invalid Replace Range: {0} - {1}", other[0].time, other[other.length - 1].time);
+                return;
+            }
+            if (curve.length == 0)
+            {
+                curve.Merge(other);
+            }
+            else
+            {
+                Vector2 frameRange = new Vector2(curve[0].time, curve[curve.length - 1].time);
+                Vector2 mergeRange = new Vector2(other[0].time, other[other.length - 1].time);
+                if (mergeRange[1] < frameRange[0] || mergeRange[0] > frameRange[1])
+                {
+                    curve.Merge(other);
+                }
+                else
+                {
+                    int startIndex = GetIndex(mergeRange.x, curve.keys);
+                    int endIndex = GetIndex(mergeRange.y, curve.keys);
+                    if (mergeRange.x == curve[startIndex].time)
+                    {
+                        startIndex -= 1;
+                    }
+                    for (int i = startIndex; i < endIndex; i++)
+                    {
+                        curve.RemoveKey(startIndex + 1);
+                    }
+                    curve.Merge(other);
+                }
+            }
         }
+
         public static float GetTimeSpan(this AnimationCurve curve)
         {
             if (curve.length <= 1) return 0;
             return curve.keys[curve.length - 1].time - curve.keys[0].time;
+        }
+
+        public static bool GetLastFrame(this AnimationCurve curve, out Keyframe keyframe)
+        {
+            if (curve.length > 0)
+            {
+                keyframe = curve.keys[curve.length - 1];
+                return true;
+            }
+            else
+            {
+                keyframe = new Keyframe();
+                return false;
+            }
+        }
+        public static void AppendKey(this AnimationCurve curve, float time, float value)
+        {
+            Keyframe key = new Keyframe(time, value);
+            int lastIndex = curve.length - 1;
+            if (curve.length > 1 && curve.keys[lastIndex].value == value && curve.keys[lastIndex - 1].value == value)
+            {
+                curve.RemoveKey(lastIndex);
+            }
+            curve.AddKey(key);
+        }
+
+        private static int GetIndex(float time, Keyframe[] frames)
+        {
+            if (frames.Length == 0 || time < frames[0].time) return 0;
+            int last = frames.Length - 1;
+            if (time >= frames[last].time) return last;
+            return GetIndex(time, frames, 0, last);
+        }
+        private static int GetIndex(float time, Keyframe[] frames, int min, int max)
+        {
+            int mid = (min + max) / 2;
+            if (mid == min) return min;
+            if (time < frames[mid].time) return GetIndex(time, frames, min, mid);
+            else if (time > frames[mid].time) return GetIndex(time, frames, mid, max);
+            else return mid;
         }
     }
 }
