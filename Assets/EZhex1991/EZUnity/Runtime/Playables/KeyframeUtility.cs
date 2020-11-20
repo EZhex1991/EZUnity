020f5ef087999196943e53ad3703786fbc764d0b
Ethan Zack <ezhex1991@outlook.com>
2020-11-20 17:07:54 +0800

    remove Obsolete functions

--- Assets/EZhex1991/EZUnity/Runtime/Playables/KeyframeUtility.cs
+++ Assets/EZhex1991/EZUnity/Runtime/Playables/KeyframeUtility.cs
@@ -47,42 +47,6 @@ namespace EZhex1991.EZUnity.Playables
                 }
             }
         }
-        public static void Replace(AnimationCurve curve, AnimationCurve other)
-        {
-            if (other.length == 0) return;
-            if (other[0].time > other[other.length - 1].time)
-            {
-                Debug.LogErrorFormat("Invalid Replace Range: {0} - {1}", other[0].time, other[other.length - 1].time);
-                return;
-            }
-            if (curve.length == 0)
-            {
-                curve.Merge(other);
-            }
-            else
-            {
-                Vector2 frameRange = new Vector2(curve[0].time, curve[curve.length - 1].time);
-                Vector2 mergeRange = new Vector2(other[0].time, other[other.length - 1].time);
-                if (mergeRange[1] < frameRange[0] || mergeRange[0] > frameRange[1])
-                {
-                    curve.Merge(other);
-                }
-                else
-                {
-                    int startIndex = GetIndex(mergeRange.x, curve.keys);
-                    int endIndex = GetIndex(mergeRange.y, curve.keys);
-                    if (mergeRange.x == curve[startIndex].time)
-                    {
-                        startIndex -= 1;
-                    }
-                    for (int i = startIndex; i < endIndex; i++)
-                    {
-                        curve.RemoveKey(startIndex + 1);
-                    }
-                    curve.Merge(other);
-                }
-            }
-        }
 
         public static int GetIndex<T>(float time, IList<T> frames) where T : IKeyframe
         {
@@ -91,23 +55,7 @@ namespace EZhex1991.EZUnity.Playables
             if (time >= frames[last].time) return last;
             return GetIndex(time, frames, 0, last);
         }
-        public static int GetIndex(float time, Keyframe[] frames)
-        {
-            if (frames.Length == 0 || time < frames[0].time) return 0;
-            int last = frames.Length - 1;
-            if (time >= frames[last].time) return last;
-            return GetIndex(time, frames, 0, last);
-        }
-
         public static int GetIndex<T>(float time, IList<T> frames, int min, int max) where T : IKeyframe
-        {
-            int mid = (min + max) / 2;
-            if (mid == min) return min;
-            if (time < frames[mid].time) return GetIndex(time, frames, min, mid);
-            else if (time > frames[mid].time) return GetIndex(time, frames, mid, max);
-            else return mid;
-        }
-        public static int GetIndex(float time, Keyframe[] frames, int min, int max)
         {
             int mid = (min + max) / 2;
             if (mid == min) return min;
